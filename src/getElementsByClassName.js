38ed9c92ed2a497c9cea4b989a1e57bc409f38a3
omega1984 <omegareborn2019@gmail.com>
2019-06-10 23:03:27 -0700

    working on JSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,18 +5,49 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
-    var array = [];
-
-	var helper = function(element){
-	  	if(element.classList && element.classList.contains(className)){
-	    	array.push(element);
-	  	}
-	    var i = 0;
-	    while(i < element.children.length){
-	      	helper(element.children[i]);
-	      	i++;
-	    }
+	var result = [];
+	function findElementsWithClassName(element){
+		if (element.classList && element.classList.contains(className)){
+			result.push(element);
+		}
+		if (element.childNodes){
+			element.childNodes.forEach(function(node){
+				findElementsWithClassName(node);
+			});
+		}
 	}
-	helper(document.body);
-	return  array;
+	findElementsWithClassName(document.body);
+	return result;
+
+ //    var array = [];
+
+	// var helper = function(element){
+	//   	if(element.classList && element.classList.contains(className)){
+	//     	array.push(element);
+	//   	}
+	//     var i = 0;
+	//     while(i < element.children.length){
+	//       	helper(element.children[i]);
+	//       	i++;
+	//     }
+	// }
+	// helper(document.body);
+	// return  array;
+	// var result = [];
+
+ //  	searchForChildNodes = function (parentNode, result, className) {
+	//     var currentLayer = parentNode.childNodes;
+	//     for (var i = 0; i < currentLayer.length; i++) {
+	//       var classList = currentLayer[i].classList;
+	//       if (classList && classList.contains(className)) {
+	//         result.push(currentLayer[i]);
+	//       }
+	//       if (currentLayer[i].childNodes.length) {
+	//         searchForChildNodes(currentLayer[i], result, className)
+	//       }
+	//     }
+
+ //  	}
+	// searchForChildNodes(document, result, className);
+	// return result;
 };
